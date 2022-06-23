2e38c1e1a625cbc2af58080109363e0c14930846
M9yk <ulasevich01@list.ru>
2022-06-23 23:02:25 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN = Math.abs(n).toString();
+    let str = "";
+    let i = strN.length - 1;
+    while (i >= 0) {
+        str = `${str}${strN[i]}`;
+        i -= 1;
+    }
+    return +str;
+};
